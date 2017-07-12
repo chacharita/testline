05b11d849929ab2a0e21ff7e457cdf3502dd6e7f
chacharita <chaz9008@gmail.com>
2017-07-12 16:03:02 +0700

    Update getmid.php

--- getmid.php
+++ getmid.php
@@ -1,38 +1,43 @@
 <?php
-// $strAccessToken = "xjGIR1MZNjzmCI9qagfTX7ksvvmLJYmOZZfCaAvY52kld2Hm4SeDJtzRv31ZDyIum31zNpHaY6lIWJ0LRzIqnxsgrBt0a+dKb56qqBmOlDtKyONakxR1kk6ADFzu3Ko5WWqxfhMcufHu3ldcWEhREAdB04t89/1O/w1cDnyilFU=";
-// $content = file_get_contents('php://input');
-
-// $arrJson = json_decode($content, true);
-
-// $strUrl = "https://api.line.me/v2/bot/message/reply";
+ $strAccessToken = "xjGIR1MZNjzmCI9qagfTX7ksvvmLJYmOZZfCaAvY52kld2Hm4SeDJtzRv31ZDyIum31zNpHaY6lIWJ0LRzIqnxsgrBt0a+dKb56qqBmOlDtKyONakxR1kk6ADFzu3Ko5WWqxfhMcufHu3ldcWEhREAdB04t89/1O/w1cDnyilFU=";
  
-// $arrHeader = array();
-// $arrHeader[] = "Content-Type: application/json";
-// $arrHeader[] = "Authorization: Bearer {$strAccessToken}";
+$content = file_get_contents('php://input');
 
-// $arrPostData = array();
-//   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-//   $arrPostData['messages'][0]['type'] = "text";
-//   $arrPostData['messages'][0]['text'] = "UserID :: ".$arrJson['events'][0]['source']['userId'];        
+$arrJson = json_decode($content, true);
+
+$strUrl = "https://api.line.me/v2/bot/message/reply";
+ 
+$arrHeader = array();
+$arrHeader[] = "Content-Type: application/json";
+$arrHeader[] = "Authorization: Bearer {$strAccessToken}";
+
+$arrPostData = array();
+  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+  $arrPostData['messages'][0]['type'] = "text";
+  $arrPostData['messages'][0]['text'] = "UserID :: ".$arrJson['events'][0]['source']['userId'];        
   
 
-// if($arrJson['events'][0]){
-//   $arrPostData = array();
-//   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-//   $arrPostData['messages'][0]['type'] = "text";
-//   $arrPostData['messages'][0]['text'] = "Hello new friend".$arrJson['events'][0];
+if($arrJson['events'][0]){
+  $arrPostData = array();
+  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+  $arrPostData['messages'][0]['type'] = "text";
+  $arrPostData['messages'][0]['text'] = "Hello new friend".$arrJson['events'][0];
   
-//  }
+ }
 
-// $ch = curl_init();
-// curl_setopt($ch, CURLOPT_URL,$strUrl);
-// curl_setopt($ch, CURLOPT_HEADER, false);
-// curl_setopt($ch, CURLOPT_POST, true);
-// curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
-// curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
-// curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
-// curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-// $result = curl_exec($ch);
-// curl_close ($ch);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,$strUrl);
+curl_setopt($ch, CURLOPT_HEADER, false);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+$result = curl_exec($ch);
+curl_close ($ch);
+ 
+?>
+
  echo "hello";
 ?>
